bfeee6c78adf0b9e41cc885019aa56da484d17a4
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-03-16 16:05:49 +0100

    [helpers] utils - new functions is_pkg_linked_to and is_file_linked_to

--- helpers/utils.bash
+++ helpers/utils.bash
@@ -13,3 +13,44 @@ success() {
 newline() {
   echo >&3
 }
+
+is_file_linked_to() {
+  local file="$1"
+  local dep="$2"
+  local linked_libs
+
+  crun bash -c "
+    ldd \"$file\"                      | \
+    awk '/=>/ {print \$3}'             | \
+    xargs pacman -Qo ${linked_libs[*]} | \
+    awk '{print \$(NF-1)}'             | \
+    grep \"$dep\"
+  "
+}
+
+is_pkg_linked_to() {
+  local pkg="$1"
+  local dep="$2"
+  local -a files
+
+  crun bash -c "
+      pkg_files=\"\$(pacman -Qlq '$pkg')\"
+      {
+          grep  -E '^/usr/bin/|\.so$' <<< \"\$pkg_files\"
+          grep -vE '^/usr/bin/|\.so$' <<< \"\$pkg_files\"
+      } | xargs -I{} sh -c 'test -f \"{}\" && echo \"{}\"'
+  "
+  mapfile -t files <<< "$output"
+
+  for f in "${files[@]}"; do
+    is_file_linked_to "$f" "$dep"
+    if [[ $status == 0 ]]; then
+      output="$pkg is linked to $dep ($f => $output)"
+      status=0
+      return
+    fi
+  done
+
+  output="$pkg is not linked to $dep"
+  status=1
+}
